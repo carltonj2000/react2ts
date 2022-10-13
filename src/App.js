ee31b66d060b76f3fed434533edf37f04dae31a3
Carlton Joseph <carlton.joseph@gmail.com>
2022-10-13 00:11:43 -0700

    add minimal ts setup for js checking

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const n = ["hi", "there", "someone"];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <ul>
+    {n.map((i,j) => <li key={j}>{i}</li>)}
+    </ul>
       </header>
     </div>
   );
